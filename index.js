d59a46fe229272f34a8607a942cbf84212db4ce9
Emily Turner <turner.ek@gmail.com>
2023-12-06 17:17:29 +0000

    responding to image prompts

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv/config');
 
-const {SlashCommandBuilder, EmbedBuilder, Client}= require('discord.js')
+const {EmbedBuilder, Client}= require('discord.js')
+
 const {configuration, OpenAI}= require('openai');
 //const translate = require('translate-google');
 const commands = require("./register-comands");
@@ -18,113 +20,58 @@ const client = new Client({
   intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent']
 });
 
-
-
    
 //create new client
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+// 
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
   
-});
 
-client.on('interactionCreate', (interaction) => {
-  if ( interaction.isChatInputCommand() ) {
-    if ( interaction.commandName === 'ping' ) {
-      interaction.reply('Pong!');
-    };
+  if (interaction.commandName === 'image-generate') {
+    await interaction.deferReply();
 
-    if ( interaction.commandName === 'info' ) {
-      interaction.reply(info);
-    };
+    
+    
+    const prompt = interaction.options.getString('prompt');
+
+   
+
+    console.log('Received prompt:', prompt);
+
+    try {
+      const response = await openai.images.generate({
+        prompt: prompt,
+        n: 1,
+        size: '1024x1024',
+      });
+
+      console.log('OpenAI API Response:', response);
+
+      const imageURL = response.data[0].url;
+
+      const embed = new EmbedBuilder()
+        .setColor('Blue')
+        .setTitle(`Here's your image of a \`\`\`${prompt}\`\`\``)
+        .setImage(imageURL)
+        .setTimestamp()
+        .setFooter({ text: 'Image Generator' });
+
+      await interaction.editReply({ embeds: [embed] });
+
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        console.error('Bad request error:', error.response.data);
+        await interaction.editReply({ content: 'Bad request error. Please provide a valid prompt.' });
+      } else {
+        console.error('Error generating image:', error);
+        await interaction.editReply({ content: 'Error generating image. Please try again later.' });
+      }
+    }
   }
 });
-
-// client.on('messageCreate', message => {
-//     console.log(`Received message: '${message.content}'`);
-  
-//     if (message.content.toLowerCase() === 'ping') {
-//       console.log("Received 'ping', replying with 'Pong!'");
-//       message.reply('Pong!');
-//     }
-//   });
-
-client.on('interactionCreate', (interaction) => {
-   
-      if (!interaction.isCommand()) return;
-  
-      if (interaction.commandName === 'hey') {
-            interaction.reply('hey!');
-      }
-    
-  });
-
-
-  
-  client.on('interactionCreate', async interaction => {
-    // Your previous code here...
-  
-    if (interaction.commandName === 'image-generate') {
-      await interaction.deferReply(); // Acknowledge receipt of the command
-  
-      const prompt = interaction.options.getString('prompt');
-  
-      try {
-        const response = await openai.images.generate({
-          prompt: `${prompt}`,
-          n: 1,
-          size: '1024x1024',
-        });
-      
-        console.log('OpenAI API Response:', response);
-      
-        // Process the response and handle the image data
-      
-        
-      
-    const imageURL = response.data[0].url;
-    
-  
-          const embed = new EmbedBuilder()
-            .setColor('Blue')
-            .setTitle(`Here's your image of a \`\`\`${prompt}\`\`\``)
-            .setImage(imageURL )
-            .setTimestamp()
-            .setFooter({ text: 'Image Generator' });
-  
-          await interaction.editReply({ embeds: [embed] });
-      
-      } catch (error) {
-        if (error.response && error.response.status === 400) {
-          console.error('Bad request error:', error.response.data);
-          await interaction.editReply({ content: 'Bad request error. Please provide a valid prompt.' });
-        } else {
-          console.error('Error generating image:', error);
-          await interaction.editReply({ content: 'Error generating image. Please try again later.' });
-        }
-      }
-    }
-  });
-  
-
-  module.exports ={
-    data: new SlashCommandBuilder()
-    .setName('image-generate')
-    .setDescription('This generates an image using a prompt prvided by you')
-    .addStringOption(Option=>Option.setName('prompt').setDescription('Describe what imagae you want to generate').setRequired(true)),
-    async execute (interaction){
-
-
-        await interaction.deferReply();
-
-        const prompt = interaction.options.getString('prompt');
-
-
-       
-    }
-
-
-}
   
   // client.on('message', async (message) => {
   //   if (message.content === '!generate-image') {
